9510db35ab3b31e88cd568061f1de2027a52e29f
Darwin Algarin <dalgarins@live.com>
2024-02-06 18:32:34 -0500

    refactor to use new configuration jmeter.

--- src/main/kotlin/co/anbora/labs/jmeter/fileTypes/JmxFileType.kt
+++ src/main/kotlin/co/anbora/labs/jmeter/fileTypes/JmxFileType.kt
@@ -1,16 +1,7 @@
 package co.anbora.labs.jmeter.fileTypes
 
 import co.anbora.labs.jmeter.ide.icons.JmeterIcons
-import co.anbora.labs.jmeter.ide.settings.Settings
-import co.anbora.labs.jmeter.ide.settings.Settings.OPTION_KEY_JMETER_PATH
 import com.intellij.openapi.fileTypes.UserBinaryFileType
-import org.apache.jmeter.JMeter
-import org.apache.jmeter.plugin.PluginManager
-import org.apache.jmeter.util.JMeterUtils
-import java.io.File
-import java.io.FileInputStream
-import java.io.IOException
-import java.util.*
 import java.util.logging.Logger
 import javax.swing.Icon
 
--- src/main/kotlin/co/anbora/labs/jmeter/ide/startup/InitConfigFiles.kt
+++ src/main/kotlin/co/anbora/labs/jmeter/ide/startup/InitConfigFiles.kt
@@ -2,7 +2,6 @@ package co.anbora.labs.jmeter.ide.startup
 
 import co.anbora.labs.jmeter.ide.actions.SetupFilesAction
 import co.anbora.labs.jmeter.ide.notifications.JMeterNotifications
-import co.anbora.labs.jmeter.ide.settings.Settings
 import co.anbora.labs.jmeter.ide.toolchain.JMeterToolchainService.Companion.toolchainSettings
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
@@ -10,7 +9,6 @@ import com.intellij.openapi.startup.ProjectActivity
 import org.apache.jmeter.JMeter
 import org.apache.jmeter.plugin.PluginManager
 import org.apache.jmeter.util.JMeterUtils
-import kotlin.io.path.exists
 
 class InitConfigFiles: ProjectActivity {
 
