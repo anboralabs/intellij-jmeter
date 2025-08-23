7a1231a4e04df07a01231364b2e321b30fcc3d64
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2025-08-22 23:45:54 -0500

    change submodules.

--- jorphan/build.gradle.kts
+++ jorphan/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    id("org.jetbrains.intellij.platform.module")
 }
 
 group = "co.anbora.labs.jmeter.jorphan"
@@ -7,11 +8,22 @@ version = "1.2.5"
 
 repositories {
     mavenCentral()
+
+    // IntelliJ Platform Gradle Plugin Repositories Extension
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
 dependencies {
+    // IntelliJ Platform Gradle Plugin Dependencies Extension
+    intellijPlatform {
+        // Create IntelliJ Platform dependency for this module to compile against the IDE APIs
+        create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
+    }
+
     // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib-jdk8
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
+    // implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.0")
 
     implementation("org.apiguardian:apiguardian-api:1.1.2")
     implementation("org.slf4j:slf4j-api:1.7.36")
@@ -21,6 +33,10 @@ dependencies {
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("org.apache.commons:commons-math3:3.6.1")
     implementation("org.apache.commons:commons-text:1.10.0")
+}
+
+intellijPlatform {
+    buildSearchableOptions = false
 }
 
 kotlin {
